15b0e5b928f113c4db1dafa0d4b80888a9ba0ee8
Bly00 <gabrielosilva1000@gmail.com>
2024-10-04 10:48:12 -0300

    push e pop na fila

--- src/Fila.java
+++ src/Fila.java
@@ -1,3 +1,40 @@
 public class Fila {
+
+    Lista fila = new Lista();
+
+
+    public void push(int novoValor){
+
+        Lista atual = fila;
+
+        System.out.println("Adicionado " + novoValor + " a fila");
+
+        atual.addElemento(novoValor, false);
+
+    }
+
+    public void pop(){
+        
+        Lista atual = fila;
+
+        System.out.println("Removido " + atual.inicio.valor + " da fila");
+
+        atual.removerElementoInicio();
+
+    }
+
+    public void mostrarFila(){
+
+        Lista atual = fila;
+        
+        System.out.println("Fila:");
+
+        while(atual.inicio != null){
+            System.out.println(atual.inicio.valor);
+            atual.inicio = atual.inicio.prox;
+        }
+
+    }
+
     
 }
